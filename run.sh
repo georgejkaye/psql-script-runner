54182db6cfff4229d4c3366ab611916774c89ace
George Kaye <georgejkaye@gmail.com>
2025-04-23 18:57:28 +0100

    fix: add password handling

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-# Set up db credentials in ~/.pgpass file
 DB_HOST=$1
 DB_PORT=$2
 DB_USER=$3
 DB_NAME=$4
 
-DATA_DIR=$5
-CODE_DIR=$6
+DB_PASSWORD=$5
+
+DATA_DIR=$6
+CODE_DIR=$7
+
+echo "$DB_HOST:$DB_PORT:$DB_NAME:$DB_USER:$DB_PASSWORD" > "~/.pgpass"
 
 for file in $DATA_DIR/*sql; do
     psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f $file
