cac12582ab9644594972aa68d6617f990b2eea04
George Kaye <georgejkaye@gmail.com>
2025-04-23 19:00:31 +0100

    fix: make pgpass properly

--- run.sh
+++ run.sh
@@ -10,6 +10,8 @@ DB_PASSWORD=$5
 DATA_DIR=$6
 CODE_DIR=$7
 
+touch "~/.pgpass"
+chmod 0600 ~/.pgpass"
 echo "$DB_HOST:$DB_PORT:$DB_NAME:$DB_USER:$DB_PASSWORD" > "~/.pgpass"
 
 for file in $DATA_DIR/*sql; do
