09c9076abe516206bae9018db692a957f0e4bebd
George Kaye <georgejkaye@gmail.com>
2025-04-23 19:02:49 +0100

    fix: use right pgpass directory

--- run.sh
+++ run.sh
@@ -10,9 +10,9 @@ DB_PASSWORD=$5
 DATA_DIR=$6
 CODE_DIR=$7
 
-touch "~/.pgpass"
-chmod 0600 ~/.pgpass"
-echo "$DB_HOST:$DB_PORT:$DB_NAME:$DB_USER:$DB_PASSWORD" > "~/.pgpass"
+touch /root/.pgpass
+chmod 0600 /root/.pgpass
+echo "$DB_HOST:$DB_PORT:$DB_NAME:$DB_USER:$DB_PASSWORD" > /root/.pgpass
 
 for file in $DATA_DIR/*sql; do
     psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f $file
